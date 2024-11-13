e7bac1540a9bc7cca92dfbebb47c6a9eede70ae0
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-11-13 11:36:48 -0300

    chore: change eslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import logo from './logo.svg'
-import './App.css'
-
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )
+import React from "react"
+import {  FunctionComponent, useState } from "react"
+interface AppProps {
+  message?:string
 }
 
+const App:FunctionComponent<AppProps> = ({message}) =>{
+  const [name,setName] = useState<string>('Judson')
+  return(
+    <>
+      <h1>{name}</h1>
+      <h2></h2>
+    </>
+  )
+
+}
 export default App
