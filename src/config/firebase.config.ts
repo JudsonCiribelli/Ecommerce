eb8fcc777956e1335cd8b0e9ef6cb33ce5c64fa7
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-11-18 10:32:42 -0300

    chore: add firebase config

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore, initializeFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBZ6oFR5C75xEMuxpIMdmqlxPHTdfHm5TA',
+  authDomain: 'clothing-ecommerce-dd1ef.firebaseapp.com',
+  projectId: 'clothing-ecommerce-dd1ef',
+  storageBucket: 'clothing-ecommerce-dd1ef.firebasestorage.app',
+  messagingSenderId: '541705371767',
+  appId: '1:541705371767:web:80f8613e4c3792f5ed0672'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
