f26a22fc4ede9f151415fd7182ebc220ee3cdbc5
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-11-26 21:36:40 -0300

    chore: initialize firebase auth

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
-import { getFirestore, initializeFirestore } from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBZ6oFR5C75xEMuxpIMdmqlxPHTdfHm5TA',
@@ -13,3 +14,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
+export const auth = getAuth(app)
