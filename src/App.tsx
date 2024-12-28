cdf6fc704a9ffa2f676cfd96596b896682233412
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-12-28 11:47:42 -0300

    fix: fixing bug when confirming payment

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ import CategoryDetailsPage from './Components/Pages/Category-details-page/Catego
 import CartComponent from './Components/Cart/Cart.component'
 import CheckOutPage from './Components/Pages/Checkout/Checkout.page'
 import AuthenticationGuard from './Guards/Authentication.guards'
-import PaymentConfirmationPage from './Components/Pages/payment-confirmation/Payment-Confirmation.Page'
+import PaymentConfirmation from './Components/Pages/payment-confirmation/Payment-Confirmation.Page'
 
 const App: FunctionComponent = () => {
   const [isInitializing, setIsInitializing] = useState(true)
@@ -69,10 +69,7 @@ const App: FunctionComponent = () => {
             </AuthenticationGuard>
           }
         />
-        <Route
-          path='/payment-confirmation'
-          element={<PaymentConfirmationPage />}
-        />
+        <Route path='/payment-confirmation' element={<PaymentConfirmation />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/signUp' element={<SignUpPage />} />
       </Routes>
